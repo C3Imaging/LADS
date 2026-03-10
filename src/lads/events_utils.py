57a49bc91e78a9646a07e8378673ba289077ba21
Paul-Kielty <56357933+Paul-Kielty@users.noreply.github.com>
2026-03-10 12:38:58 +0000

    Refactor activity to score

--- src/lads/events_utils.py
+++ src/lads/events_utils.py
@@ -222,13 +222,9 @@ class FixedDurationEventReader:
                     return event_window
         raise StopIteration
 
-def grid_tensor_to_img(integrated, clip_val=5, draw_heatmap=False, draw_grid=False, annotate_score=False, annotate_decay=False, 
+def grid_tensor_to_img(grid, patch_scores, patch_decay_factors, clip_val=5, draw_heatmap=False, draw_grid=False, annotate_score=False, annotate_decay=False, 
                        write_path=None, recursive=False, text_scale=0.8):
-        if isinstance(integrated, tuple):
-            grid = integrated[0]
-            patch_activities, patch_decay_factors = integrated[1]
-        else:
-            grid = integrated
+        
         if not isinstance(clip_val, tuple):
             clip_val = (-clip_val,clip_val)
         
@@ -241,88 +237,87 @@ def grid_tensor_to_img(integrated, clip_val=5, draw_heatmap=False, draw_grid=Fal
         frame_orig = frame.copy()
         height, width = frame.shape[0], frame.shape[1]
 
-        if isinstance(integrated, tuple):
-            if draw_grid or annotate_score or annotate_decay:
-                line_colour = (100, 255, 100)
-                if not recursive:
-                    patch_size = height//patch_activities.shape[0]
-                    patch_activities = patch_activities.detach().cpu().numpy()
-                    # patch_activities_laplace = filters.laplace(patch_activities)
-                    # patch_activities = local_info_gain(patch_activities)
-                    num_patches_y, num_patches_x = patch_activities.shape[-2], patch_activities.shape[-1]
-                    # if len(patch_activities.shape) > 2:
-                    #     _, num_patches_y, num_patches_x = patch_activities.shape[0], patch_activities.shape[1]
-                    # else:
-                    # num_patches_y, num_patches_x = patch_activities.shape[1], patch_activities.shape[2]
-                    for i in range(num_patches_y):
-                        #draw horizontal patch lines at 50% opacity
-                        patch_y = i*patch_size
-                        cv2.line(frame, (0, patch_y), (width, patch_y), line_colour, 1)
+        if draw_grid or annotate_score or annotate_decay:
+            line_colour = (100, 255, 100)
+            if not recursive:
+                patch_size = height//patch_scores.shape[0]
+                patch_scores = patch_scores.detach().cpu().numpy()
+                # patch_scores_laplace = filters.laplace(patch_scores)
+                # patch_scores = local_info_gain(patch_scores)
+                num_patches_y, num_patches_x = patch_scores.shape[-2], patch_scores.shape[-1]
+                # if len(patch_scores.shape) > 2:
+                #     _, num_patches_y, num_patches_x = patch_scores.shape[0], patch_scores.shape[1]
+                # else:
+                # num_patches_y, num_patches_x = patch_scores.shape[1], patch_scores.shape[2]
+                for i in range(num_patches_y):
+                    #draw horizontal patch lines at 50% opacity
+                    patch_y = i*patch_size
+                    cv2.line(frame, (0, patch_y), (width, patch_y), line_colour, 1)
 
-                        for j in range(num_patches_x):
-                            # annotate patch activity and decay factor
-                            patch_x = j*patch_size
-                            cv2.line(frame, (patch_x, 0), (patch_x, height), line_colour, 1)
+                    for j in range(num_patches_x):
+                        # annotate patch score and decay factor
+                        patch_x = j*patch_size
+                        cv2.line(frame, (patch_x, 0), (patch_x, height), line_colour, 1)
+                        
+                        if patch_size >= 30: #otherwise too small for text, just use heatmap
+                            if annotate_score:
+                                patch_score = patch_scores[i,j]
+                                cv2.putText(frame, f"{patch_score:.2f}", (patch_x+5, patch_y+25), cv2.FONT_HERSHEY_SIMPLEX, text_scale, (255,0,0), 1)
                             
-                            if patch_size >= 30: #otherwise too small for text, just use heatmap
-                                if annotate_score:
-                                    patch_activity = patch_activities[i,j]
-                                    cv2.putText(frame, f"{patch_activity:.2f}", (patch_x+5, patch_y+25), cv2.FONT_HERSHEY_SIMPLEX, text_scale, (255,0,0), 1)
-                                
-                                if annotate_decay:
-                                    patch_decay_factor = patch_decay_factors[patch_y+patch_size//2,patch_x+patch_size//2].item()
-                                    # Get color from colormap based on decay value
-                                    decay_color_val = np.clip(patch_decay_factor, 0, 1)
-                                    decay_color_gray = np.array([[[decay_color_val * 255]]], dtype=np.uint8)
-                                    decay_color_mapped = cv2.applyColorMap(decay_color_gray, cv2.COLORMAP_JET)[0,0]
-                                    # Convert BGR to RGB and make it a tuple for OpenCV
-                                    text_color = (int(decay_color_mapped[0]), int(decay_color_mapped[1]), int(decay_color_mapped[2]))
-                                    cv2.putText(frame, f"{patch_decay_factor:.2f}", (patch_x+5, patch_y+50 if annotate_score else patch_y+25), cv2.FONT_HERSHEY_SIMPLEX, text_scale, text_color, 1)
-                else:
+                            if annotate_decay:
+                                patch_decay_factor = patch_decay_factors[patch_y+patch_size//2,patch_x+patch_size//2].item()
+                                # Get color from colormap based on decay value
+                                decay_color_val = np.clip(patch_decay_factor, 0, 1)
+                                decay_color_gray = np.array([[[decay_color_val * 255]]], dtype=np.uint8)
+                                decay_color_mapped = cv2.applyColorMap(decay_color_gray, cv2.COLORMAP_JET)[0,0]
+                                # Convert BGR to RGB and make it a tuple for OpenCV
+                                text_color = (int(decay_color_mapped[0]), int(decay_color_mapped[1]), int(decay_color_mapped[2]))
+                                cv2.putText(frame, f"{patch_decay_factor:.2f}", (patch_x+5, patch_y+50 if annotate_score else patch_y+25), cv2.FONT_HERSHEY_SIMPLEX, text_scale, text_color, 1)
+            else:
 
-                    patch_size = height//patch_activities.shape[0]
-                    patch_activities = patch_activities.detach().cpu().numpy()
-                    num_patches_y, num_patches_x = patch_activities.shape[-2], patch_activities.shape[-1]
+                patch_size = height//patch_scores.shape[0]
+                patch_scores = patch_scores.detach().cpu().numpy()
+                num_patches_y, num_patches_x = patch_scores.shape[-2], patch_scores.shape[-1]
 
-                    for i in range(num_patches_y):
-                        patch_y = i*patch_size
+                for i in range(num_patches_y):
+                    patch_y = i*patch_size
 
-                        for j in range(num_patches_x):
-                            patch_x = j*patch_size
-                            
-                            patch_activity = patch_activities[i,j]
-                            diff_from_above = False
-                            diff_from_left = False
-                            # Draw patch lines where activity is different to neighbour (for visualisation of recursive subdivision)
-                            if i > 0:
-                                if patch_activity != patch_activities[i-1,j]:
-                                    diff_from_above = True
-                                    # draw horizontal line on top of patch
-                                    cv2.line(frame, (patch_x, patch_y), (patch_x+patch_size, patch_y), line_colour, 1)
-                            if j > 0:
-                                if patch_activity != patch_activities[i,j-1]:
-                                    diff_from_left = True
-                                    # draw vertical line on left of patch
-                                    cv2.line(frame, (patch_x, patch_y), (patch_x, patch_y+patch_size), line_colour, 1)
-                            if patch_size >= 30 and annotate_score: #otherwise too small for text, just use heatmap
-                                if (diff_from_above and diff_from_left) or (diff_from_left and i == 0) or (diff_from_above and j == 0) or (i == 0 and j == 0):
-                                    cv2.putText(frame, f"{patch_activity:.2f}", (patch_x+2, patch_y+14), cv2.FONT_HERSHEY_SIMPLEX, text_scale, (255,0,0), 1)
+                    for j in range(num_patches_x):
+                        patch_x = j*patch_size
+                        
+                        patch_score = patch_scores[i,j]
+                        diff_from_above = False
+                        diff_from_left = False
+                        # Draw patch lines where score is different to neighbour (for visualisation of recursive subdivision)
+                        if i > 0:
+                            if patch_score != patch_scores[i-1,j]:
+                                diff_from_above = True
+                                # draw horizontal line on top of patch
+                                cv2.line(frame, (patch_x, patch_y), (patch_x+patch_size, patch_y), line_colour, 1)
+                        if j > 0:
+                            if patch_score != patch_scores[i,j-1]:
+                                diff_from_left = True
+                                # draw vertical line on left of patch
+                                cv2.line(frame, (patch_x, patch_y), (patch_x, patch_y+patch_size), line_colour, 1)
+                        if patch_size >= 30 and annotate_score: #otherwise too small for text, just use heatmap
+                            if (diff_from_above and diff_from_left) or (diff_from_left and i == 0) or (diff_from_above and j == 0) or (i == 0 and j == 0):
+                                cv2.putText(frame, f"{patch_score:.2f}", (patch_x+2, patch_y+14), cv2.FONT_HERSHEY_SIMPLEX, text_scale, (255,0,0), 1)
 
-                                # patch_decay_factor = patch_decay_factors[patch_y, patch_x].item()
-                                # cv2.putText(frame, f"{patch_decay_factor:.2f}", (patch_x+2, patch_y+30), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,0,0), 1)
-                frame = frame_orig*.65 + frame*.35
-            if draw_heatmap:
-                decay_grid = patch_decay_factors.detach().cpu().numpy()
-                decay_grid = np.clip(decay_grid, 0, 1)
-                decay_grid = np.stack((decay_grid, decay_grid, decay_grid), axis=2)
+                            # patch_decay_factor = patch_decay_factors[patch_y, patch_x].item()
+                            # cv2.putText(frame, f"{patch_decay_factor:.2f}", (patch_x+2, patch_y+30), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,0,0), 1)
+            frame = frame_orig*.65 + frame*.35
+        if draw_heatmap:
+            decay_grid = patch_decay_factors.detach().cpu().numpy()
+            decay_grid = np.clip(decay_grid, 0, 1)
+            decay_grid = np.stack((decay_grid, decay_grid, decay_grid), axis=2)
 
-                decay_grid = 1-decay_grid  # invert for heatmap
-                decay_grid = (decay_grid * 255).astype(np.uint8)
-                decay_grid = cv2.applyColorMap(decay_grid, cv2.COLORMAP_JET)
-                frame = 0.9*frame + 0.1*decay_grid
-                
-                frame = frame.astype(np.uint8)
-    
+            decay_grid = 1-decay_grid  # invert for heatmap
+            decay_grid = (decay_grid * 255).astype(np.uint8)
+            decay_grid = cv2.applyColorMap(decay_grid, cv2.COLORMAP_JET)
+            frame = 0.9*frame + 0.1*decay_grid
+            
+            frame = frame.astype(np.uint8)
+
         if write_path is not None:
             cv2.imwrite(write_path, frame)
         return frame
--- src/lads/leaky_integrator.py
+++ src/lads/leaky_integrator.py
@@ -9,7 +9,7 @@ class LADS:
                  do_patch_decay=False, patch_size=None, interpolate_patches=True,
                  min_decay=None,
                  fft_filter_radius=0.05,
-                 recursive=False, min_patch_size=0, activity_threshold=0.5, stop_above_thresh=True):
+                 recursive=False, min_patch_size=0, score_threshold=0.5, stop_above_thresh=True):
         
         self.device = device
         self.W = W
@@ -24,7 +24,7 @@ class LADS:
         self.reference_event_rate = reference_event_rate
         self.recursive = recursive
         self.min_patch_size = min_patch_size
-        self.activity_threshold = activity_threshold
+        self.score_threshold = score_threshold
         self.stop_above_thresh = stop_above_thresh
         self.fft_filter_radius = fft_filter_radius
         self.min_decay = min_decay if min_decay is not None else 0 # Minimum decay factor to apply, if given
@@ -41,9 +41,9 @@ class LADS:
         if self.do_patch_decay and self.patch_size is not None:
             if isinstance(self.patch_size, int):
                 self.patch_size = (self.patch_size, self.patch_size)    
-            self.patch_activity_conv = torch.nn.Conv2d(1, 1, kernel_size=self.patch_size, stride=self.patch_size, padding=0, bias=False, device=device)           
-            self.patch_activity_conv.weight.data.fill_(1)
-            self.patch_activity_conv.weight.requires_grad = False
+            self.patch_score_conv = torch.nn.Conv2d(1, 1, kernel_size=self.patch_size, stride=self.patch_size, padding=0, bias=False, device=device)           
+            self.patch_score_conv.weight.data.fill_(1)
+            self.patch_score_conv.weight.requires_grad = False
             self.reference_event_rate = reference_event_rate*self.patch_size[0]*self.patch_size[1] # Event rate that sets centres decay at self.decay
         else:
             self.reference_event_rate = reference_event_rate*H*W # Event rate that sets centres decay at self.decay
@@ -55,7 +55,7 @@ class LADS:
 
         if self.decay_func == "log":
             self.laplace_kernel_025 = create_log_kernel(3, 0.25, device=device)
-            self.patch_activity_conv.weight.data.fill_(1/(self.patch_size[0]*self.patch_size[1]))
+            self.patch_score_conv.weight.data.fill_(1/(self.patch_size[0]*self.patch_size[1]))
 
         if self.decay_func == "fft":
             patch_filters = {}
@@ -81,8 +81,8 @@ class LADS:
     '''
 
     def calc_patch_event_rates(self, events, time_diff_s):
-        activity_voxel_grid = voxel(events, self.H, self.W, self.device, polarity_mapping=(1, 1))
-        patch_event_counts = self.patch_activity_conv(activity_voxel_grid).squeeze(0)
+        score_voxel_grid = voxel(events, self.H, self.W, self.device, polarity_mapping=(1, 1))
+        patch_event_counts = self.patch_score_conv(score_voxel_grid).squeeze(0)
         patch_event_rates = patch_event_counts/time_diff_s
         return patch_event_rates
     
@@ -92,53 +92,53 @@ class LADS:
             if time_diff_s <= 0:
                 return 1, 0
             event_rate = len(events) / time_diff_s
-            activity = event_rate / self.reference_event_rate
-            decay_factor = np.exp((-(self.decay*activity)))
-            return decay_factor, activity
+            score = event_rate / self.reference_event_rate
+            decay_factor = np.exp((-(self.decay*score)))
+            return decay_factor, score
         else:
             if time_diff_s <= 0:
                 return torch.ones((self.H//self.patch_size[0], self.W//self.patch_size[1]), device=self.device), \
                        torch.zeros((self.H//self.patch_size[0], self.W//self.patch_size[1]), device=self.device)
             patch_event_rates = self.calc_patch_event_rates(events, time_diff_s)
-            patch_activities = patch_event_rates/self.reference_event_rate
-            # decay scales exonentially with activity, but unlike event-rate-linear, does not hit 1 when equal to reference rate
+            patch_scores = patch_event_rates/self.reference_event_rate
+            # decay scales exonentially with score, but unlike event-rate-linear, does not hit 1 when equal to reference rate
             # when equal to reference rate, decay is exp(-decay)
-            patch_decay_factors = torch.exp(((-time_diff_s*patch_activities)/self.decay))
-            return patch_decay_factors, patch_activities
+            patch_decay_factors = torch.exp(((-time_diff_s*patch_scores)/self.decay))
+            return patch_decay_factors, patch_scores
     
     def decay_by_event_rate_linear(self, events, time_diff_s, use_patches=False):
         if not use_patches:
             if time_diff_s <= 0:
                 return 1, 0
             event_rate = len(events) / time_diff_s
-            activity = event_rate / self.reference_event_rate
-            decay_factors = 1 - activity
-            return decay_factors, activity
+            score = event_rate / self.reference_event_rate
+            decay_factors = 1 - score
+            return decay_factors, score
         else:
             if time_diff_s <= 0:
                 return torch.ones((self.H//self.patch_size[0], self.W//self.patch_size[1]), device=self.device), \
                        torch.zeros((self.H//self.patch_size[0], self.W//self.patch_size[1]), device=self.device)
             patch_event_rates = self.calc_patch_event_rates(events, time_diff_s)
-            patch_activities = patch_event_rates/self.reference_event_rate
-            patch_decay_factors = 1 - patch_activities
-            return patch_decay_factors, patch_activities
+            patch_scores = patch_event_rates/self.reference_event_rate
+            patch_decay_factors = 1 - patch_scores
+            return patch_decay_factors, patch_scores
     
 
     '''
     ============================= Laplace-of-Gaussian =============================
     '''
     def decay_by_LoG(self, new_grid):
-        activities = torch.abs(conv2d(new_grid.unsqueeze(0).unsqueeze(0), self.laplace_kernel_025, padding=1))
+        scores = torch.abs(conv2d(new_grid.unsqueeze(0).unsqueeze(0), self.laplace_kernel_025, padding=1))
         if self.patch_size[0] == 1 and self.patch_size[1] == 1:
-            patch_activities = activities.squeeze(0).squeeze(0)
+            patch_scores = scores.squeeze(0).squeeze(0)
         else:
-            patch_means = self.patch_activity_conv(activities).squeeze(0).squeeze(0)
-            patch_activities = patch_means
+            patch_means = self.patch_score_conv(scores).squeeze(0).squeeze(0)
+            patch_scores = patch_means
         
         a = self.falloff_rate
         b = self.decay#*100
-        patch_decay_factors = (1.0 + np.exp(-a*(b))) / (1.0 + torch.exp(a * (patch_activities - b)))
-        return patch_decay_factors, patch_activities
+        patch_decay_factors = (1.0 + np.exp(-a*(b))) / (1.0 + torch.exp(a * (patch_scores - b)))
+        return patch_decay_factors, patch_scores
 
     
     
@@ -183,33 +183,33 @@ class LADS:
         R = torch.where(torch.isnan(R), 1, R)
         return R
 
-    def decay_by_fft(self, activity_voxel_grid):
+    def decay_by_fft(self, score_voxel_grid):
 
         if self.patch_size is None:
             if self.recursive:
-                patch_activities = subdivide_grid_recur(activity_voxel_grid, min_patch_size=self.min_patch_size, activity_function=self.get_fft_fraction,
-                                                        activity_threshold=self.activity_threshold, stop_above_thresh=self.stop_above_thresh)
+                patch_scores = subdivide_grid_recur(score_voxel_grid, min_patch_size=self.min_patch_size, score_function=self.get_fft_fraction,
+                                                        score_threshold=self.score_threshold, stop_above_thresh=self.stop_above_thresh)
             else:
                 raise ValueError("Full-frame FFT decay not currently supported, please provide a patch_size or set recursive=True.")
 
         else:
             ph, pw = self.patch_size
             ny, nx = self.H // ph, self.W // pw
-            patches = activity_voxel_grid.unsqueeze(0).unsqueeze(0).unfold(2, self.patch_size[0], self.patch_size[0]).unfold(3, self.patch_size[1], self.patch_size[1]).contiguous()
+            patches = score_voxel_grid.unsqueeze(0).unsqueeze(0).unfold(2, self.patch_size[0], self.patch_size[0]).unfold(3, self.patch_size[1], self.patch_size[1]).contiguous()
             patches = patches.view(1,1,ny,nx,self.patch_size[0],self.patch_size[1])
                 
             if self.recursive:
-                patch_activities = self.recurr_with_init_patches(patches)
+                patch_scores = self.recurr_with_init_patches(patches)
 
             else:
-                patch_activities = torch.zeros((ny, nx), device=self.device)
+                patch_scores = torch.zeros((ny, nx), device=self.device)
                 for i in range(ny):
                     for j in range(nx):
                         new_grid_patch = patches[:,:,i,j]
-                        patch_activity = self.get_fft_fraction(new_grid_patch)
-                        patch_activities[i,j] = patch_activity
+                        patch_score = self.get_fft_fraction(new_grid_patch)
+                        patch_scores[i,j] = patch_score
 
-        return patch_activities
+        return patch_scores
 
         
     '''
@@ -257,12 +257,12 @@ class LADS:
                 
             if not self.do_patch_decay:
                 if self.decay_func == "er":
-                    decay_factor, activity = self.decay_by_event_rate_exp(events, time_diff_s, use_patches=False)
+                    decay_factor, score = self.decay_by_event_rate_exp(events, time_diff_s, use_patches=False)
                     decay_factor = torch.tensor(decay_factor, device=self.device).clamp(0, 1-self.min_decay)
                     return self._update_grid(decay_factor, new_events=events)
                 
                 elif self.decay_func == "event-rate-linear":
-                    decay_factor, activity = self.decay_by_event_rate_linear(events, time_diff_s, use_patches=False)
+                    decay_factor, score = self.decay_by_event_rate_linear(events, time_diff_s, use_patches=False)
                     decay_factor = torch.tensor(decay_factor, device=self.device).clamp(0, 1-self.min_decay)
                     return self._update_grid(decay_factor, new_events=events)
 
@@ -275,20 +275,20 @@ class LADS:
             new_grid = voxel(events, self.H, self.W, self.device, polarity_mapping=(-1, 1)).squeeze(0)
 
             if self.decay_func == "er":
-                patch_decay_factors, patch_activities = self.decay_by_event_rate_exp(events, time_diff_s, use_patches=True)
+                patch_decay_factors, patch_scores = self.decay_by_event_rate_exp(events, time_diff_s, use_patches=True)
 
 
             elif self.decay_func == "er-linear":
-                patch_decay_factors, patch_activities = self.decay_by_event_rate_linear(events, time_diff_s, use_patches=True)
+                patch_decay_factors, patch_scores = self.decay_by_event_rate_linear(events, time_diff_s, use_patches=True)
 
 
             elif self.decay_func == "log":
-                patch_decay_factors, patch_activities = self.decay_by_LoG(new_grid)
+                patch_decay_factors, patch_scores = self.decay_by_LoG(new_grid)
 
 
             elif self.decay_func == "fft":
-                patch_activities = self.decay_by_fft(new_grid)
-                patch_decay_factors = patch_activities
+                patch_scores = self.decay_by_fft(new_grid)
+                patch_decay_factors = patch_scores
 
 
 
@@ -299,8 +299,8 @@ class LADS:
                                                                     mode='bilinear', align_corners=False).squeeze(0).squeeze(0)
                                                                     # mode='bicubic', align_corners=False).squeeze(0).squeeze(0)
             else:
-                patch_decay_factors = patch_decay_factors.repeat_interleave(self.H//patch_activities.shape[-2], dim=0)
-                patch_decay_factors = patch_decay_factors.repeat_interleave(self.W//patch_activities.shape[-1], dim=1)
+                patch_decay_factors = patch_decay_factors.repeat_interleave(self.H//patch_scores.shape[-2], dim=0)
+                patch_decay_factors = patch_decay_factors.repeat_interleave(self.W//patch_scores.shape[-1], dim=1)
 
             # Clamp decay factors
             patch_decay_factors = patch_decay_factors.clamp(0,1-self.min_decay)
@@ -308,10 +308,7 @@ class LADS:
             # Apply decay factors to old grid
             self.old_grid = new_grid + self.old_grid*patch_decay_factors
 
-            if return_patch_features:
-                return self.old_grid, (patch_activities, patch_decay_factors)
-                
-            return self.old_grid
+            return self.old_grid, patch_scores, patch_decay_factors
     
     def recurr_with_init_patches(self, patches):
         ph, pw = patches.shape[-2:]
@@ -321,16 +318,16 @@ class LADS:
         for i in range(ny):
             for j in range(nx):
                 new_grid_patch = patches[:,:,i,j] # Add batch and channel dimensions
-                patch_activity = self.get_fft_fraction(new_grid_patch)
-                if (patch_activity < self.activity_threshold and self.stop_above_thresh) or \
-                    (patch_activity > self.activity_threshold and not self.stop_above_thresh):
+                patch_score = self.get_fft_fraction(new_grid_patch)
+                if (patch_score < self.score_threshold and self.stop_above_thresh) or \
+                    (patch_score > self.score_threshold and not self.stop_above_thresh):
                     if not (ph%2 != 0 or pw%2 != 0 or ph/2 < self.min_patch_size or pw/2 < self.min_patch_size): 
 
-                        patch_activity = subdivide_grid_recur(new_grid_patch[0,0], min_patch_size=self.min_patch_size, 
-                                                                activity_function=self.get_fft_fraction,
-                                                                activity_threshold=self.activity_threshold, 
+                        patch_score = subdivide_grid_recur(new_grid_patch[0,0], min_patch_size=self.min_patch_size, 
+                                                                score_function=self.get_fft_fraction,
+                                                                score_threshold=self.score_threshold, 
                                                                 stop_above_thresh=self.stop_above_thresh)
-                new_powers.append(patch_activity)
+                new_powers.append(patch_score)
                 new_is.append((i,j))
 
         max_shape = max([p.shape[0] for p in new_powers])
@@ -345,11 +342,11 @@ class LADS:
                     p = p.repeat_interleave(2, dim=0).repeat_interleave(2, dim=1)
                 new_powers[p_i] = p
 
-        patch_activities = torch.zeros((max_shape*ny,max_shape*nx), device=self.device)
+        patch_scores = torch.zeros((max_shape*ny,max_shape*nx), device=self.device)
         for i in range(ny):
             for j in range(nx):
-                patch_activities[max_shape*i:max_shape*(i+1),max_shape*j:max_shape*(j+1)] = new_powers[new_is.index((i,j))]
-        return patch_activities
+                patch_scores[max_shape*i:max_shape*(i+1),max_shape*j:max_shape*(j+1)] = new_powers[new_is.index((i,j))]
+        return patch_scores
                         
 
 
